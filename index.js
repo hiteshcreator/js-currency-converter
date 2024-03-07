bc90a2491b3fe84589d81607e87eb69c3009e521
hiteshcreator <thehiteshcreator@gmail.com>
2024-03-07 15:06:12 +0530

    initial update 2.0

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+import { validCurrencyCodes } from './validCurrencyCode.js';
+import 'dotenv/config';
 
-const freecurrencyapi = new Freecurrencyapi('fca_live_MxfjMdsyFLtu2mGxTphElqcwFJ30VpBnC4AyVXgt');
+let freecurrencyapi; // Define the freecurrencyapi variable globally
+
+const initializeFreecurrencyapi = (apiKey) => {
+    return new Freecurrencyapi(apiKey);
+};
+
+export const setApiKey = (apiKey) => {
+    // Update the API key used by freecurrencyapi instance
+    freecurrencyapi = initializeFreecurrencyapi(apiKey || process.env.FREECURRENCYAPI_KEY);
+};
 
 export const convertCurrency = async(fromCurrency, toCurrency, units) =>{
+        try {
+                // Use a safer validation function (e.g., from a trusted library)
+                if (!validateCurrencyCode(fromCurrency) || !validateCurrencyCode(toCurrency)) {
+                        throw new Error('Invalid currency code(s)');
+                }
 
-        const res = await freecurrencyapi.latest({
-                        base_currency: fromCurrency,
-                        currencies: toCurrency
-                    });
-        const data = res.data[toCurrency];
-        return data*units;
+                const res = await freecurrencyapi.latest({
+                                base_currency: fromCurrency,
+                                currencies: toCurrency
+                        });
+
+                if (!res.data || !res.data[toCurrency]) {
+                        throw new Error('Conversion data not found');
+                }
+
+                const conversionRate = res.data[toCurrency];
+
+                if (isNaN(conversionRate)) {
+                        throw new Error('Invalid conversion rate');
+                }
+
+                return conversionRate*units;
+        } catch (error) {
+                console.error('Error during currency conversion:', error);
+        }
 };
+
+const validateCurrencyCode = (code) => {
+        // Check if the provided code exists in the validCurrencyCodes array
+        return validCurrencyCodes.includes(code);
+}
